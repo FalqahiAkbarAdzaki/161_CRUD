0505f6ad52212bf6543d87938edbdce371ce4572
FalqahiAkbarAdzaki <dragontamer1925@gmail.com>
2025-10-21 14:36:23 +0700

    menambahkan error message untuk api

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-Const express = require("express");
+const express = require("express");
 let mysql = require("mysql2");
 const app = express();
 const port = 3000;
@@ -27,4 +27,35 @@ db.connect((err) => {
     return;
   }
   console.log("Koneksi Berhasil.");
+});
+
+app.get('/api/mahasiswa', (req, res) => {
+    db.query("SELECT * FROM biodata", (err, results) => {
+        if (err) {
+            console.error("Error executing query:" + err.stack);
+            res.status(500).send("Error fetching users");
+            return;
+        }
+        res.json(results);
+    });
+});
+
+app.post('/api/mahasiswa', (req, res) => {
+    const {nama, alamat, agama} = req.body;
+
+    if (!nama || !alamat || !agama) {
+        return res.status(400).json9({ message : "Nama, alamat, dan agama harus diisi"});
+    }
+
+    db.query(
+        "INSERT INTO biodata (nama, alamat, agama) VALUES (?, ?, ?)",
+        [nama, alamat, agama],
+        (err, results) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ message: "database error" });
+            }
+            res.status(201).json({ message: "user created succesfully"});
+        }
+    );
 });
